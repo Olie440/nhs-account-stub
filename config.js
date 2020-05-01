7cb7c8b4558592208196b7d39e8288ba42400dbc
Oliver Rushworth <oliverrushworth@olivers-mbp.home>
2020-05-01 14:39:10 +0100

    Fix the Stub for the new backend structure

--- config.js
+++ config.js
@@ -18,6 +18,20 @@ const PreFlightResults = {
   },
 };
 
+const SubmissionResults = {
+  GO_TO_AUTO: 'StartFaceScan',
+  GO_TO_MANUAL: 'ReferredToManual',
+  FAIL: null
+}
+
+const P5UpliftResponses = {
+  SUCCESS: { status: 200, body: { message:"Uplift Successful" } },
+  NO_MATCH: { status: 404, body: { error: "No match found" } },
+  NOT_VERIFIED: { status: 404, body: { error: "NHS number not verified" }},
+  MULTIPLE: { status: 404, body: { error: "Multiple matches found" }},
+  OTHER: { status: 503, body: { error: "Internal server error" } }
+};
+
 module.exports = {
   // Port to run the stub on
   PORT: 4000,
@@ -25,8 +39,12 @@ module.exports = {
   PYI_FLOW_TYPE: FlowTypes.AUTO,
   // Preflight flow to use
   PYI_PREFLIGHT_CHECK_RESULT: PreFlightResults.BLURRED,
+  // Result of photo submission
+  PYI_SUBMISSION_RESULT: SubmissionResults.GO_TO_AUTO,
+  // P5 flow to use
+  P5_UPLIFT_RESPONSE: P5UpliftResponses.NO_MATCH,
   // Where to redirect to after login is successful
-  SUCCESS_REDIRECT_URL: "http://localhost:4200/proofing/know-nhs-number",
+  SUCCESS_REDIRECT_URL: "http://localhost:4200/#/patient-online/gp-connect",
   // This shouldn't need to change as it expires in 2030, it can be edited using https://jwt.io/
   ID_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZjhhZjExZC04OTgwLTRhNWUtOGNjYy1iYTc1Yjc3NzEwYmUiLCJhY2NvdW50X2lkIjoiZGY4YWYxMWQtODk4MC00YTVlLThjY2MtYmE3NWI3NzcxMGJlIiwiY3VzdG9tOmFjY291bnRfaWQiOiJkZjhhZjExZC04OTgwLTRhNWUtOGNjYy1iYTc1Yjc3NzEwYmUiLCJpc3MiOiJodHRwczovL2F1dGguZGV2LnNpZ25pbi5uaHMudWsiLCJpYXQiOjE1ODM5MjA2MTksImV4cCI6MTg5MzQ1NjAwMCwiYXVkIjoiaHR0cHM6Ly9hdXRoLmRldi5zaWduaW4ubmhzLnVrIiwianRpIjoiMDM0ZjQ1M2ItOTZhOS00OThiLWI2ZTEtYzA4MGVkMGI4ZmRhIiwiYXV0aGVudGljYXRpb25fdmVjdG9yIjoiQ3AuQ2QiLCJyZW1lbWJlcl9teV9kZXZpY2UiOmZhbHNlLCJ1c2VyX2FnZW50IjoiQW1hem9uIENsb3VkRnJvbnQifQ.H_mqSUYEKHcGk62HgJ2mDKls9s9CKXUnAHrw9jlb4_U"
 };
--- index.js
+++ index.js
@@ -3,95 +3,12 @@ const cors = require("cors");
 const app = express();
 
 const config = require('./config');
-const addSpaLoginRoutes = require('./lib/spa-login');
-const addModuleAccountRoutes = require('./lib/module-account');
+const addLoginRoutes = require('./lib/login');
+const addPyiRoutes = require('./lib/pyi');
 
 app.use(cors({ credentials: true, origin: true }));
 
-addSpaLoginRoutes(app);
-addModuleAccountRoutes(app);
-
-app.get("/pyi/pol/:any/status", (_, res) => {
-  res.send({
-    status: "RequiresTriage"
-  });
-  res.end();
-});
-
-app.post("/pyi/initialisation/:any", (_, res) => {
-  res.send({
-    flowType: config.FLOW_TYPE,
-    initialisationOutcome: "StartPyi"
-  });
-  res.end();
-});
-
-app.get("/client-info", (_, res) => {
-  res.send({
-    client_id: "nhs-online",
-    client_display_name: "NHS App",
-    approved_scopes: [
-      "openid",
-      "profile",
-      "nhs_app_credentials",
-      "gp_integration_credentials",
-      "phone",
-      "email",
-      "profile_extended"
-    ],
-    claims: [
-      "sub",
-      "birthdate",
-      "family_name",
-      "nhs_number",
-      "im1_token",
-      "gp_ods_code",
-      "gp_user_id",
-      "gp_linkage_key",
-      "gp_integration_credentials",
-      "phone_number",
-      "phone_number_verified",
-      "email",
-      "email_verified",
-      "given_name"
-    ]
-  });
-  res.end();
-});
-
-app.get("/pyi/upload/url/:any/:photoType", (_, res) => {
-  res.send({
-    signed_url: `http://localhost:${config.PORT}/unsubmitted/1`,
-    validation_id: "1"
-  });
-  res.end();
-});
-
-app.put("/unsubmitted/1", (_, res) => {
-  res.status(200);
-  res.end();
-});
-
-app.get("/pyi/upload/validation/:object_key", (_, res) => {
-  res.send({
-    validationStatus: "success",
-    validationMessage: "",
-    validationRejectReason: "",
-    ...config.PREFLIGHT_CHECK_RESULT
-  });
-  res.end();
-});
-
-app.put("/pyi/verification/:any/document/submission", (_, res) => {
-  res.send({
-    error: null
-  });
-  res.end();
-});
-
-app.post("/account-logger/logs", (_, res) => {
-  res.status(200);
-  res.end();
-});
+addLoginRoutes(app);
+addPyiRoutes(app);
 
 app.listen(config.PORT, () => console.log("Stub Started on port", config.PORT));
