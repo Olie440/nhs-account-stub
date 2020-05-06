d84f868cbf31ae76b20ef0b59cab287c3fa42171
Oliver Rushworth <oliverrushworth@Olivers-MacBook-Pro.local>
2020-05-06 12:32:59 +0100

    Add a start page that sets the needed cookies

--- config.js
+++ config.js
@@ -32,9 +32,23 @@ const P5UpliftResponses = {
   OTHER: { status: 503, body: { error: "Internal server error" } }
 };
 
+const OIDC_PARAMS = {
+  scope:'openid profile nhs_app_credentials gp_integration_credentials phone email profile_extended',
+  response_type:'code',
+  client_id:'stub',
+  redirect_uri:'http://localhost:3000/auth-return',
+  session_id:'db3abe87-57f6-4c60-962f-67ea0d9f27ed',
+  state:'appstate',
+  nonce:'randomnonce',
+  remember_my_device:false, 
+  vtr: '["Cp.Cd","Cp.Ck","Cm"]'
+};
+
 module.exports = {
   // Port to run the stub on
   PORT: 4000,
+  // Address of the local front-end
+  FRONT_END_URL: 'http://localhost:4200/',
   // Flow to use
   PYI_FLOW_TYPE: FlowTypes.AUTO,
   // Preflight flow to use
@@ -44,7 +58,9 @@ module.exports = {
   // P5 flow to use
   P5_UPLIFT_RESPONSE: P5UpliftResponses.NO_MATCH,
   // Where to redirect to after login is successful
-  SUCCESS_REDIRECT_URL: "http://localhost:4200/#/patient-online/gp-connect",
+  SUCCESS_REDIRECT_ROUTE: "#/patient-online/gp-connect",
   // This shouldn't need to change as it expires in 2030, it can be edited using https://jwt.io/
-  ID_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZjhhZjExZC04OTgwLTRhNWUtOGNjYy1iYTc1Yjc3NzEwYmUiLCJhY2NvdW50X2lkIjoiZGY4YWYxMWQtODk4MC00YTVlLThjY2MtYmE3NWI3NzcxMGJlIiwiY3VzdG9tOmFjY291bnRfaWQiOiJkZjhhZjExZC04OTgwLTRhNWUtOGNjYy1iYTc1Yjc3NzEwYmUiLCJpc3MiOiJodHRwczovL2F1dGguZGV2LnNpZ25pbi5uaHMudWsiLCJpYXQiOjE1ODM5MjA2MTksImV4cCI6MTg5MzQ1NjAwMCwiYXVkIjoiaHR0cHM6Ly9hdXRoLmRldi5zaWduaW4ubmhzLnVrIiwianRpIjoiMDM0ZjQ1M2ItOTZhOS00OThiLWI2ZTEtYzA4MGVkMGI4ZmRhIiwiYXV0aGVudGljYXRpb25fdmVjdG9yIjoiQ3AuQ2QiLCJyZW1lbWJlcl9teV9kZXZpY2UiOmZhbHNlLCJ1c2VyX2FnZW50IjoiQW1hem9uIENsb3VkRnJvbnQifQ.H_mqSUYEKHcGk62HgJ2mDKls9s9CKXUnAHrw9jlb4_U"
+  ID_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZjhhZjExZC04OTgwLTRhNWUtOGNjYy1iYTc1Yjc3NzEwYmUiLCJhY2NvdW50X2lkIjoiZGY4YWYxMWQtODk4MC00YTVlLThjY2MtYmE3NWI3NzcxMGJlIiwiY3VzdG9tOmFjY291bnRfaWQiOiJkZjhhZjExZC04OTgwLTRhNWUtOGNjYy1iYTc1Yjc3NzEwYmUiLCJpc3MiOiJodHRwczovL2F1dGguZGV2LnNpZ25pbi5uaHMudWsiLCJpYXQiOjE1ODM5MjA2MTksImV4cCI6MTg5MzQ1NjAwMCwiYXVkIjoiaHR0cHM6Ly9hdXRoLmRldi5zaWduaW4ubmhzLnVrIiwianRpIjoiMDM0ZjQ1M2ItOTZhOS00OThiLWI2ZTEtYzA4MGVkMGI4ZmRhIiwiYXV0aGVudGljYXRpb25fdmVjdG9yIjoiQ3AuQ2QiLCJyZW1lbWJlcl9teV9kZXZpY2UiOmZhbHNlLCJ1c2VyX2FnZW50IjoiQW1hem9uIENsb3VkRnJvbnQifQ.H_mqSUYEKHcGk62HgJ2mDKls9s9CKXUnAHrw9jlb4_U",
+  // Contents of nhs-authorization-cookie to be set, edit above
+  NHS_COOKIE: JSON.stringify(OIDC_PARAMS)
 };
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const config = require('./config');
 const addLoginRoutes = require('./lib/login');
 const addPyiRoutes = require('./lib/pyi');
 
+const { ID_TOKEN, NHS_COOKIE, FRONT_END_URL } = require('./config');
+
 app.use(cors({ credentials: true, origin: true }));
+app.use(cookieParser());
 
 addLoginRoutes(app);
 addPyiRoutes(app);
 
-app.listen(config.PORT, () => console.log("Stub Started on port", config.PORT));
+app.get('/', (_, res) => {
+    res.cookie('id_token', ID_TOKEN);
+    res.cookie('nhs-authorization-cookie', NHS_COOKIE);
+    res.redirect(302, FRONT_END_URL);
+});
+
+app.listen(config.PORT, () => console.log('Stub Started on port', config.PORT));
